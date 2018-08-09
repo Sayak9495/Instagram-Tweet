9373ab8576913179877196df2d5ace8f1391c488
Sayak Sen <sayak1997sen@gmail.com>
2018-08-09 13:35:43 +0530

    test1

--- tests/test_insta_tweet.py
+++ tests/test_insta_tweet.py
@@ -10,16 +10,16 @@ from click.testing import CliRunner
 from insta_tweet import insta_tweet
 from insta_tweet import cli
 
-class TestInsta_tweet(unittest.TestCase):
+class test_insta_tweet(unittest.TestCase):
     """Tests for `insta_tweet` package."""
 
-    #def testapi_info(self):
-    #    response=insta_tweet.insta_tweet('godemperormusk')
-    #    self.assertEqual(response,"DONE")
-
-    def test_insta_tweet(self):
+    def testapi_info(self):
         response=insta_tweet.insta_tweet('godemperormusk')
         self.assertEqual(response,"DONE")
+
+    #def test_insta_tweet(self):
+    #    response=insta_tweet.insta_tweet('godemperormusk')
+    #    self.assertEqual(response,"DONE")
 
 if __name__ == '__main__':
     unittest.main()
